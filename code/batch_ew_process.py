cea0bb635cf25ac9db4112500c9c10a59e123207
gully <igully@gmail.com>
2021-02-10 14:39:08 -0600

    Run the Black code formatting

--- code/batch_ew_process.py
+++ code/batch_ew_process.py
@@ -20,6 +20,7 @@ warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 calcium_line = 8542
 
+
 def get_goldilocks_dataframe(fn):
     """Return a pandas Dataframe given a Goldilocks FITS file name"""
     hdus = fits.open(fn)
@@ -30,11 +31,11 @@ def get_goldilocks_dataframe(fn):
         for i in range(1, 10):
             name = hdus[i].name
             df[name] = hdus[i].data[j, :]
-        df['order'] = j
+        df["order"] = j
         df_original = df_original.append(df, ignore_index=True)
     keep_mask = df_original[df_original.columns[0:6]] != 0.0
     df_original = df_original[keep_mask.all(axis=1)].reset_index(drop=True)
-    
+
     return df_original, header
 
 
@@ -42,36 +43,43 @@ def normalize_spectrum(df):
     """Normalizes spectrum to set to one"""
     for order in df.order.unique():
         mask = df.order == order
-        norm_constant = df['Sci Flux'][mask].median() #mean takes outliers into account
-        df['Sci Flux'][mask] = df['Sci Flux'][mask]/norm_constant
-        df['Sci Error'][mask] = df['Sci Error'][mask]/norm_constant
-        
+        norm_constant = df["Sci Flux"][
+            mask
+        ].median()  # mean takes outliers into account
+        df["Sci Flux"][mask] = df["Sci Flux"][mask] / norm_constant
+        df["Sci Error"][mask] = df["Sci Error"][mask] / norm_constant
+
     return df
 
-def generative_model(m, b, A, mu, logw, int_wl = calcium_line):
+
+def generative_model(m, b, A, mu, logw, int_wl=calcium_line):
     """Generate the model given parameters"""
     continuum = m * (wl - int_wl) + b
     w = np.exp(logw)
-    gaussian = A * np.exp(-0.5*(wl-mu)**2/w**2)
+    gaussian = A * np.exp(-0.5 * (wl - mu) ** 2 / w ** 2)
     return continuum - gaussian
+
 
 def log_likelihood(theta):
     m, b, A, mu, logw = theta
-    model = generative_model(m, b, A, mu, logw, int_wl = calcium_line)
+    model = generative_model(m, b, A, mu, logw, int_wl=calcium_line)
     residual = flux - model
-    chi_squared = np.sum(residual** 2 / unc**2)
+    chi_squared = np.sum(residual ** 2 / unc ** 2)
     return -0.5 * chi_squared
 
 
 def main():
-    sampler = emcee.EnsembleSampler(n_walkers, n_params, log_likelihood)#, pool=pool)
-            #kwargs={'wl':wl, 'flux':flux, 'unc':unc}, threads=12)
+    sampler = emcee.EnsembleSampler(n_walkers, n_params, log_likelihood)  # , pool=pool)
+    # kwargs={'wl':wl, 'flux':flux, 'unc':unc}, threads=12)
     sampler.run_mcmc(pos, n_steps, progress=False)
 
     return sampler
 
-if __name__ == '__main__':
-    goldilocks_files = glob.glob('../data/HPF/Helium-transit-data/**/Goldilocks*.fits', recursive=True)
+
+if __name__ == "__main__":
+    goldilocks_files = glob.glob(
+        "../data/HPF/Helium-transit-data/**/Goldilocks*.fits", recursive=True
+    )
 
     order = 4
     n_walkers = 32
@@ -85,45 +93,61 @@ if __name__ == '__main__':
     for index in tqdm(range(0, len(goldilocks_files))):
 
         fn = goldilocks_files[index]
-        
+
         try:
             df_orig, header = get_goldilocks_dataframe(fn)
-            date_raw = header['DATE-OBS']
+            date_raw = header["DATE-OBS"]
             date = date_raw[0:10]
             time = date_raw[11:19]
-            obj = header['OBJECT']
+            obj = header["OBJECT"]
             df = normalize_spectrum(df_orig)
-            qidx = header['QIDX']
+            qidx = header["QIDX"]
             j_date = date_raw
-            t = Time(j_date, format='isot', scale='utc')
+            t = Time(j_date, format="isot", scale="utc")
             jd = t.jd
 
             wavelength1 = 8538
             wavelength2 = 8546
             calcium_line = 8542
 
-            sub_region = (df.order == order) & (df['Sci Wavl'] > wavelength1) & (df['Sci Wavl'] < wavelength2)
-            wl = df['Sci Wavl'][sub_region].values
-            flux = df['Sci Flux'][sub_region].values
-            unc = df['Sci Error'][sub_region].values
+            sub_region = (
+                (df.order == order)
+                & (df["Sci Wavl"] > wavelength1)
+                & (df["Sci Wavl"] < wavelength2)
+            )
+            wl = df["Sci Wavl"][sub_region].values
+            flux = df["Sci Flux"][sub_region].values
+            unc = df["Sci Error"][sub_region].values
 
-            m_guess, b_guess, A_guess, mu_guess, logw_guess = 0.01, 0.3, 0.1, calcium_line, np.log(0.4)
+            m_guess, b_guess, A_guess, mu_guess, logw_guess = (
+                0.01,
+                0.3,
+                0.1,
+                calcium_line,
+                np.log(0.4),
+            )
             theta_guess = np.array([m_guess, b_guess, A_guess, mu_guess, logw_guess])
 
-            pos = theta_guess + 1e-4 * np.random.randn(n_walkers, n_params) #intial guess position
+            pos = theta_guess + 1e-4 * np.random.randn(
+                n_walkers, n_params
+            )  # intial guess position
 
-            #with Pool() as pool:
+            # with Pool() as pool:
             sampler = main()
 
             flat_samples = sampler.get_chain(discard=1000, thin=15, flat=True)
 
-            A_draws = flat_samples[:,2]
-            b_draws = flat_samples[:,1]
-            m_draws = flat_samples[:,0]
-            mu_draws = flat_samples[:,3]
+            A_draws = flat_samples[:, 2]
+            b_draws = flat_samples[:, 1]
+            m_draws = flat_samples[:, 0]
+            mu_draws = flat_samples[:, 3]
             w_draws = np.exp(flat_samples[:, 4])
 
-            EW = ((2*np.pi)**.5)*(A_draws*w_draws)/(m_draws*(mu_draws-calcium_line)+b_draws)
+            EW = (
+                ((2 * np.pi) ** 0.5)
+                * (A_draws * w_draws)
+                / (m_draws * (mu_draws - calcium_line) + b_draws)
+            )
             EW
 
             ew_mean = np.mean(EW)
@@ -132,22 +156,30 @@ if __name__ == '__main__':
             gauss_width_unc = np.std(w_draws)
             obs_line_center = np.mean(mu_draws)
             obs_line_center_unc = np.std(mu_draws)
-            #print(ew_mean)
-            #print(ew_std)
-            temp = {'ew':ew_mean, 'ew_unc':ew_std, 'date':date, 'star_name':obj, 
-                    'time':time, 'int_wv':calcium_line, 'qidx':qidx, 'jd':jd,
-                    'gaussian_width':gauss_width, 'gaussian_width_unc':gauss_width_unc,
-                    'obs_line_center':obs_line_center, 'obs_line_center_unc':obs_line_center_unc}
+            # print(ew_mean)
+            # print(ew_std)
+            temp = {
+                "ew": ew_mean,
+                "ew_unc": ew_std,
+                "date": date,
+                "star_name": obj,
+                "time": time,
+                "int_wv": calcium_line,
+                "qidx": qidx,
+                "jd": jd,
+                "gaussian_width": gauss_width,
+                "gaussian_width_unc": gauss_width_unc,
+                "obs_line_center": obs_line_center,
+                "obs_line_center_unc": obs_line_center_unc,
+            }
 
             # save the dataframe every 10 spectra
             df_results = df_results.append(temp, ignore_index=True)
             if (index % 10) == 0:
-                #print(index, fn[-49:])
-                df_results.to_csv('../data/preliminary_results.csv',index=False)
+                # print(index, fn[-49:])
+                df_results.to_csv("../data/preliminary_results.csv", index=False)
         except:
             print("Fail:", fn)
             problem_files[index] = fn
             raise
 
-
-    
